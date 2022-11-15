479f25e7d742bfeafb0609ca3c2942066455f08e
jan <janwanja.wagener@yahoo.de>
2022-11-15 18:20:47 +0100

    Updated Test in main()-method

--- src/pgdp/convexhull/QuickHull.java
+++ src/pgdp/convexhull/QuickHull.java
@@ -92,7 +92,7 @@ public class QuickHull {
 	}
 
 	public static void main(String[] args) {
-		int[][] in = new int[][] { { 0, 0 }, { 0, 1 }, { 0, 2 }, { 0, 3 } };
+		int[][] in = new int[][] { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
 		int[][] hull = quickHull(in);
 		System.out.println(pointsToPlotString(in));
 		System.out.println(pointsToPlotString(hull));
